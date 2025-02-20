e11644f7ffadb826a795abf42d0e582f6c40c924
lyang <1465858518@qq.com>
2025-02-20 11:14:21 +0800

    1

--- picture.py
+++ picture.py
@@ -10,32 +10,46 @@ sys.path.append(r"D:\Software\python\pythonProject\Optically-pumped-atoms\Optica
 import numpy as np
 import matplotlib.pyplot as plt
 import scienceplots
+import mpl_toolkits.axisartist as axisartist
 
 
-t=np.arange(0,6*np.pi,0.01)
+t=np.arange(0,4*np.pi,0.01)
 y1=np.sin(t/2)**2
 y2=1-y1
 
 plt.style.use(['science'])
 with plt.style.context(['science']):
     plt.rc('font',family='Times New Roman')
+    #通过set_visible方法设置绘图区所有坐标轴隐藏
     fig=plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    p1,=ax.plot(t,y1,color='black')
-    p2,=ax.plot(t,y2,color='black',linestyle='dashed')
-    plt.xlabel('$\Omega t$')
-    plt.ylabel('$P$')
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
-    ax.tick_params(axis='x', which='both', bottom=True, top=False, labelbottom=True, labeltop=False)
-    ax.tick_params(axis='y', which='both', left=True, right=False, labelleft=True, labelright=False)
-    ax.set_xticks([0, 2*np.pi,4*np.pi, 6*np.pi])
-    ax.set_yticks([0,0.5, 1])
-    ax.set_xticklabels(['$0$', '$2\pi$', '$4\pi$', '$6\pi$'], rotation=0, fontsize=12)
-    ax.legend([p1, p2],["$ P_g$", "$ P_e$"],loc='upper right')
-    plt.ylim([0,1.3])
-    plt.savefig('rabi.png', dpi=600)
+    #使用axisartist.Subplot方法创建一个绘图区对象ax
+    ax = axisartist.Subplot(fig, 111)  
+    #将绘图区对象添加到画布中
+    fig.add_axes(ax)
+    ax.axis[:].set_visible(False)
+    #ax.new_floating_axis代表添加新的坐标轴
+    ax.axis["x"] = ax.new_floating_axis(0,0)
+    #给x坐标轴加上箭头
+    ax.axis["x"].set_axisline_style("->", size = 1.0)
+    #添加y坐标轴，且加上箭头
+    ax.axis["y"] = ax.new_floating_axis(1,0)
+    ax.axis["y"].set_axisline_style("-|>", size = 1.0)
+    #设置x、y轴上刻度显示方向
+    ax.axis["x"].set_axis_direction("bottom")
+    ax.axis["y"].set_axis_direction("left")
 
+    p1,=ax.plot(t,y1,color='orangered')
+    p2,=ax.plot(t,y2,color='orangered',linestyle='dashed')
+    ax.axis["x"].set_label('$\Omega t$')
+    ax.axis["y"].set_label('$P$')
+
+    ax.set_xticks([0, 2*np.pi,4*np.pi])
+    ax.set_yticks([0,0.5, 1])
+    ax.set_xticklabels(['$0$', '$2\pi$', '$4\pi$'], rotation=0, fontsize=12)
+    ax.legend([p1, p2],["$ P_g$", "$ P_e$"],loc='upper right',ncol=2)
+    plt.ylim([0,1.2])
+    plt.savefig('rabi.png', dpi=600)
+plt.show()
 
 
 
