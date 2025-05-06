a86321b116062a1040cd7a005bf46378d148d4ed
YangLiup <163539476+YangLiup@users.noreply.github.com>
2025-05-06 21:34:24 +0800

    1

---
+++ SERF_magnetometer_simulation.py
@@ -0,0 +1,123 @@
+
+import sys
+sys.path.append(r"/Users/liyang/Documents/GitHub/Optically_polarized_atoms/my_functions")
+
+import matplotlib.pyplot as plt
+from spin_operators_of_2or1_alkali_metal_atoms import spin_operators_of_2or1_alkali_metal_atoms
+from alkali_atom_uncoupled_to_coupled import alkali_atom_uncoupled_to_coupled
+from qutip import *
+from scipy.linalg import *
+import numpy as np
+from matplotlib import ticker
+from tqdm import trange
+import scienceplots
+from scipy import signal
+
+
+def master_equation(I,Rse,omega_0,Rop,Rsd,T):
+    a = round(I + 1 / 2)
+    b = round(I - 1 / 2)
+
+    # --------------------------------Generate the angular momentum operators-----------------------------------#
+    U = alkali_atom_uncoupled_to_coupled(round(2 * I))
+    ax, ay, az, bx, by, bz = spin_operators_of_2or1_alkali_metal_atoms(1, I)
+    Sx = np.kron(np.eye(round(2 * I + 1)), np.array(1 / 2 * sigmax().full()))
+    Sx = U.T.conjugate() @ Sx @ U
+    Sy = np.kron(np.eye(round(2 * I + 1)), np.array(1 / 2 * sigmay().full()))
+    Sy = U.T.conjugate() @ Sy @ U
+    Sz = np.kron(np.eye(round(2 * I + 1)), np.array(1 / 2 * sigmaz().full()))
+    Sz = U.T.conjugate() @ Sz @ U
+
+    # --------------------------------Characterize interactions envolved-----------------------------------#
+    
+    theta_op=np.pi/2
+    sx=np.sin(theta_op)
+    sz=np.cos(theta_op)
+    # --------------------------------Define the initial state-----------------------------------#
+    theta = np.pi / 4
+    phi = np.pi / 4
+    a_theta = spin_Jx(a) * np.sin(theta) * np.cos(phi) + spin_Jy(a) * np.sin(theta) * np.sin(phi) + spin_Jz(a) * np.cos(
+        theta)
+    b_theta = spin_Jx(b) * np.sin(theta) * np.cos(phi) + spin_Jy(b) * np.sin(theta) * np.sin(phi) + spin_Jz(b) * np.cos(
+        theta)
+    qa, va = np.linalg.eig(np.array(a_theta.full()))
+    qb, vb = np.linalg.eig(np.array(b_theta.full()))
+    v = block_diag(va, vb)
+    q = np.hstack((qa, qb))
+    Rho_ini = np.zeros(2 * (a + b + 1))
+
+    # # -----------------spin temperature state-----------------#
+    P = 1e-10
+    beta = np.log((1 + P) / (1 - P))
+    for i in np.arange(0, 2 * (a + b + 1), 1):
+        Rho_ini = Rho_ini + np.exp(beta * q[i]) * v[:, [i]] * v[:, [i]].T.conjugate()
+    Rho_ini = Rho_ini / np.trace(Rho_ini)
+
+    # -----------------eigenstates-----------------#
+
+    # Rho_ini = np.outer(np.array([0, 1, 0, 0, 0, 0, 0, 0]), np.array([0, 1, 0, 0, 0, 0, 0, 0]))
+
+    # --------------------------------------Evolution under hyperfine effect, etc.--------------------------------#
+    Rhot = Rho_ini
+    hyperfine = block_diag(np.ones((2 * a + 1, 2 * a + 1)), np.ones((2 * b + 1, 2 * b + 1)))  # 一个原子
+    Py = np.zeros(round(T / dt))
+
+    H = omega_0 * (az - bz)  # 投影定理
+    q, v = np.linalg.eig(H)
+    evolving_B = v @ np.diag(np.exp(-1j * q * dt)) @ np.linalg.inv(v)
+    for n in trange(0, round(T / dt), 1):
+        x1 = Rhot @ Sx
+        x2 = Rhot @ Sy
+        x3 = Rhot @ Sz
+        AS = 3 / 4 * Rhot - (Sx @ x1 + Sy @ x2 + Sz @ x3)
+        alpha = Rhot - AS
+        mSx = np.trace(x1)
+        mSy = np.trace(x2)
+        mSz = np.trace(x3)
+        mSS = mSx * Sx + mSy * Sy + mSz * Sz
+        ER = -Rsd * AS
+
+        OP = Rop[n] * (2 * alpha @ (sx*Sx+sz*Sz) - AS)
+
+        Rhot = evolving_B @ Rhot @ evolving_B.T.conjugate()  # Zeeman effect
+
+        Rhot = Rse * (alpha + 4 * alpha @ mSS - Rhot) * dt + (
+                ER + OP) * dt + Rhot
+        Rhot = hyperfine * Rhot
+        # -----------------Observables-----------------#
+        Py[n] = np.trace(2*Sy@Rhot)
+    return Py
+
+
+global dt
+dt=0.01
+T=800
+t=np.arange(0,T,dt)
+
+frequency = 0.1
+amplitude = 0.5
+Rop= 0.1*amplitude * signal.square(2 * np.pi * frequency * t)+0.5
+
+Rsd =0.1 
+
+Py=master_equation(3/2,1,0.01,Rop,Rsd,T)
+
+plt.style.use(['science'])
+with plt.style.context(['science']):
+    plt.rc('font',family='Times New Roman')
+    fig = plt.figure()
+    
+    ax1 = fig.add_subplot(111)
+    ax1.plot(t,Py)
+    # ax1.plot(tt1, Maz)
+    # ax1.set_xlim([0,50000])
+    # ax1.set_ylim([0,1])
+    ax1.set_ylabel('$P_y$', fontsize=8)
+    ax1.set_xlabel('$t(1/R_{\\text{se}})$', fontsize=8)
+    # ax1.set_xlim(1500,2000)
+    # ax1.set_ylim(-1e-3,-5e-4)
+    ax1.tick_params(axis='both', which='major', labelsize=8)
+    ax1.tick_params(axis='both', which='minor', labelsize=8)
+    plt.grid()
+    plt.savefig('signal.png', dpi=1000)
+plt.show()
