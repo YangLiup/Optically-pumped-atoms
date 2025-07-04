7122f1f4f3b1b03216232ea196886df836bd7a1d
lyang <1465858518@qq.com>
2025-07-04 10:44:17 +0800

    1

--- heating/Coil_constant.py
+++ heating/Coil_constant.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 z0=20e-3
 def single_coil_constant(a,b,z):
+    """
+    一个长a，宽b的矩形线圈框在轴线坐标轴z0处在单位电流下产生的磁场大小
+    """
     mu0=4*np.pi*1e-7
     I=1 #安培
     B=mu0*I/(4*np.pi)*(4*a*b/(a**2+4*z**2)+4*a*b/(b**2+4*z**2))/np.sqrt(a**2+b**2+4*z**2)
@@ -20,6 +23,9 @@ def multi_coil_constant(n,z0,line_spacing):
     return total_B
 
 def two_layers_coil_constant(n,layer_spacing):
+    """
+    两层加热膜，膜与膜之间的距离为layer_spacing
+    """
     return multi_coil_constant(n,z0,line_spacing)-multi_coil_constant(n,z0+layer_spacing,line_spacing)
 
 # a=np.arange(0,100e-3,1e-3)
@@ -27,6 +33,9 @@ def two_layers_coil_constant(n,layer_spacing):
 # plt.plot(a,single_coil_constant(a,a,20e-3))
 # plt.show()
 
+# """
+# 两层加热膜产生的磁噪声大小
+# """
 mu0=4*np.pi*1e-7
 z0=20e-3
 layer_spacing=0.2e-3
@@ -38,9 +47,9 @@ n=32
 delta_B=delta_current*two_layers_coil_constant(n,layer_spacing)
 print('两层',n,'匝方形线圈加热膜的磁噪声为', delta_B)
 
-"""
-Johnson热电压噪声, nV量级
-"""
+# """
+# Johnson热电压噪声, nV量级
+# """
 T=473  # K
 kB=1.38e-23 #J/K
 R=100
